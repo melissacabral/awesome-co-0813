6dbe1f1ceb4757b61c6c9a1201f1a2b538e3078e
Melissa Cabral <melissacabral@gmail.com>
2013-08-28 17:53:49 -0700

    first version

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php 
+//activate post featured images
+add_theme_support( 'post-thumbnails' );
+
+//add custom image sizes
+add_image_size( 'awesome-home-banner', 960, 330, true );
+add_image_size( 'awesome-interior-banner', 960, 113, true );
+
+//don't close PHP
